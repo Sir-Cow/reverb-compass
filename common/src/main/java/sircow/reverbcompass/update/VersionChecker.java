615703cb06a80ed076d5267447e34b24cb8edb65
Sir-Cow <16866289+Sir-Cow@users.noreply.github.com>
2026-06-17 06:21:25 +0100

    1.1.0
    - remove custom update checker
    - refactor custom trigger
    - update multiloader template

--- common/src/main/java/sircow/reverbcompass/update/VersionChecker.java
+++
@@ -1,5 +0,0 @@
-package sircow.reverbcompass.update;
-
-public interface VersionChecker {
-    String getVersion();
-}
--- neoforge/src/main/java/sircow/reverbcompass/ReverbCompass.java
+++ neoforge/src/main/java/sircow/reverbcompass/ReverbCompass.java
@@ -6,13 +6,11 @@ import sircow.reverbcompass.components.NeoforgeModComponents;
 import sircow.reverbcompass.recipe.NeoforgeModRecipes;
 import sircow.reverbcompass.sound.NeoforgeModSounds;
 import sircow.reverbcompass.trigger.NeoForgeModTriggers;
-import sircow.reverbcompass.update.NeoforgeVersionChecker;
 
 @Mod(Constants.MOD_ID)
 public class ReverbCompass {
     public ReverbCompass(IEventBus eventBus) {
         CommonClass.init();
-        Constants.INSTANCE = new NeoforgeVersionChecker();
         NeoforgeModComponents.registerNeoForgeModComponents(eventBus);
         NeoforgeModRecipes.registerNeoForgeModRecipes(eventBus);
         NeoforgeModSounds.registerNeoForgeModSounds(eventBus);
